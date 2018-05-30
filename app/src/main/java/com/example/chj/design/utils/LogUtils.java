a7a2ef8ed37e6bed6678d48ff7ae00c3e026dbf3
chj1987 <1907564608@qq.com>
2018-05-30 16:57:21 +0800

    Logger

--- app/src/main/java/com/example/chj/design/utils/LogUtils.java
+++ app/src/main/java/com/example/chj/design/utils/LogUtils.java
@@ -29,51 +29,51 @@ public class LogUtils {
 
     public static void d(String tag, String msg) {
 
-        Logger.t(tag).d(tag, msg);
+        Logger.t(tag).d(msg);
 
     }
 
     public static void d(String tag, String msg, Throwable throwable) {
 
 
-        Logger.t(tag).d(tag, msg, throwable);
+        Logger.t(tag).d(msg, throwable);
 
 
     }
 
     public static void i(String tag, String msg) {
 
-        Logger.t(tag).i(tag, msg);
+        Logger.t(tag).i(msg);
 
     }
 
     public static void i(String tag, String msg, Throwable throwable) {
 
-        Logger.t(tag).i(tag, msg, throwable);
+        Logger.t(tag).i(msg, throwable);
 
     }
 
     public static void w(String tag, String msg) {
 
-        Logger.t(tag).w(tag, msg);
+        Logger.t(tag).w(msg);
 
     }
 
     public static void w(String tag, String msg, Throwable throwable) {
 
-        Logger.t(tag).w(tag, msg, throwable);
+        Logger.t(tag).w(msg, throwable);
 
     }
 
     public static void e(String tag, String msg) {
 
-        Logger.t(tag).e(tag, msg);
+        Logger.t(tag).e(msg);
 
     }
 
     public static void e(String tag, String msg, Throwable throwable) {
 
-        Logger.t(tag).e(tag, msg, throwable);
+        Logger.t(tag).e(msg, throwable);
 
     }
 
