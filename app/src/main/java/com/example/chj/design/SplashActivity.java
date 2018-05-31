947b1e46273f1734dd79ed5dd750dec91080d4ff
chj1987 <1907564608@qq.com>
2018-05-31 14:35:49 +0800

    lottie

--- app/src/main/java/com/example/chj/design/SplashActivity.java
+++ app/src/main/java/com/example/chj/design/SplashActivity.java
@@ -1,18 +1,49 @@
 package com.example.chj.design;
 
+import android.animation.Animator;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+
+import com.airbnb.lottie.LottieAnimationView;
 
 /**
  * Created by ff on 2018/5/30.
  */
 
 public class SplashActivity extends AppCompatActivity {
+
+    private LottieAnimationView view;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        view = (LottieAnimationView) findViewById(R.id.lottie_layer_name);
+        view.setAnimation("LottieLogo2.json");
+        view.loop(false);
+        view.playAnimation();
+        view.addAnimatorListener(new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animator) {
 
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animator) {
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animator) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animator) {
+
+            }
+        });
     }
 }
